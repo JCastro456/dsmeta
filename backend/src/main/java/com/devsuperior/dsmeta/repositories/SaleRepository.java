29b957b7ebddd1d5b5a40195e1f2a1cbf5aa0f3e
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2022-07-14 14:21:07 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {	
 	
 	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
